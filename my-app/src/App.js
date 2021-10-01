4e8409e6c608b0586c0916546e1ac01787e0915a
merardosalas <msalascas69@gmail.com>
2021-09-30 21:24:42 -0500

    Subir app

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './navegacion/NavBar';
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
